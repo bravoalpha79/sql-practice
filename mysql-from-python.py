4a67be32f359db5c5146ca7e607b0a4fa3678514
Igor Basuga <igor.basuga@gmail.com>
2020-02-28 11:51:50 +0000

    add code to script

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -3,16 +3,17 @@ import pymysql
 
 username = os.getenv("C9_USER")
 
-connection = pymysql.connect (host="localhost",
-                            user=username,
-                            password="",
-                            db="Chinook")
+connection = pymysql.connect(host="localhost",
+                             user=username,
+                             password="",
+                             db="Chinook")
 
 try:
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)                   
+        list_of_names = ["Jim", "Bob"]
+        format_strings = ",".join(["%s"]*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});"
+                       .format(format_strings), list_of_names)
+        connection.commit()
 finally:
     connection.close()
